205a755204a54ba2130eb00b21f99f8029329d0a
Your Name <you@example.com>
2024-09-22 11:24:41 +0530

    covered some basic fundamentals 03

--- plain_node-package/react.js
+++ plain_node-package/react.js
@@ -12,14 +12,47 @@ const roott = ReactDOM.createRoot(document.getElementById('roott'));
 // let childd =React.createElement('h1',{},'Hello from parent child h1');
 // let child = React.createElement('div',{id:'child'},React.createElement('h1',{},'Hello from parent child h1'));
 // let parent = React.createElement('div',{id:'parent',style:{background:'red',color:'white'}},child);
-let parent =
-React.createElement('div', { id: 'parent', style: { background: 'red', color: 'white' } },
-  [React.createElement('div', { id: 'child' },
-  [React.createElement('h1', {}, 'Hello from parent child h1'),
-     React.createElement('h2', {}, 'Hello from parent child h2')]),
-   React.createElement('div', { id: 'child1' },
-    [React.createElement('h1', {}, 'Hello from parent child h1'),
-      React.createElement('h2', {}, 'Hello from parent child h2')]
-  )]);
-console.log(parent);
-roott.render(parent);
+// let parent =
+// React.createElement('div', { id: 'parent', style: { background: 'red', color: 'white' } },
+//   [React.createElement('div', { id: 'child' },
+//   [React.createElement('h1', {}, 'Hello from parent child h1'),
+//      React.createElement('h2', {}, 'Hello from parent child h2')]),
+//    React.createElement('div', { id: 'child1' },
+//     [React.createElement('h1', {}, 'Hello from parent child h1'),
+//       React.createElement('h2', {}, 'Hello from parent child h2')]
+//   )]);
+
+
+//  using jsx trans-piled by babel
+// let parent = <div id="parent" style="background: red; color: white;"><div id="child"><h1>Hello from parent child h1</h1><h2>Hello from parent child h2</h2></div><div id="child1"><h1>Hello from parent child h1</h1><h2>Hello from parent child h2</h2></div></div>;
+// let parent =
+//   (<div id="parent" style="background: red; color: white;">
+//     <div id="child">
+//       <h1>Hello from parent child h1</h1>
+//       <h2>Hello from parent child h2</h2>
+//     </div>
+//     <div id="child1">
+//       <h1>Hello from parent child h1</h1>
+//       <h2>Hello from parent child h2</h2>
+//     </div>
+//   </div>);
+
+
+// using functional component and functional composition
+const Child = ({id})=>{
+  console.log({id})
+  return(<div id="child">
+    <h1>Hello from parent {id}</h1>
+    <h2>Hello from child {id}</h2>
+  </div>)
+}
+const Parent = () =>{
+  return(<div id="parent" style={{background:'red',color:'white'}}>
+  <Child id='1'/>
+  <Child id ='2'/>
+  {Child({id:'3'})}
+  <Child id="4"></Child>
+  </div>)
+}
+console.log(<Parent/>);
+roott.render(<Parent/>);
