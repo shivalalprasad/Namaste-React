c363f02728b7fb1f8284aea52569ced19519aabf
Your Name <you@example.com>
2024-09-23 18:39:39 +0530

    started using apis

--- food-app/src/components/Main.js
+++ food-app/src/components/Main.js
@@ -1,24 +1,35 @@
 import FoodCard from "./FoodCard"
-import { restaurentList } from "../util/dummydata"
-import { useState } from "react"
+// import { restaurentList } from "../util/dummydata"
+import { useEffect, useState } from "react"
 
 const Main = () => {
-  let [restaurentlist, setRestaurentList] = useState(restaurentList);
+  let [restaurentlist, setRestaurentList] = useState([]);
+  // console.log(restaurentList)
+  // console.log("restaurent")
+  useEffect(() => { fetchData() }, [])
+
+  var fetchData = async () => {
+    var data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.8320833&lng=78.7603726&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    var json = await data.json();
+    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setRestaurentList(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  }
+
   return (
     <>
       <div className="flex justify-center mx-auto flex-wrap mb-10">
         <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none tracking-wider cursor-pointer border-indigo-500 bg-gray-100  rounded-t text-indigo-500"
-        onClick={()=>{setRestaurentList(restaurentList)}}
+          onClick={fetchData}
         >
           All
         </a>
         <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none tracking-wider cursor-pointer border-gray-200"
-        onClick={() => {
-          const topRated=restaurentlist.filter((restaurentList)=> restaurentList.info.avgRatingString > 4)
-          console.log(topRated);
-          setRestaurentList(topRated);
-         }
-         }>
+          onClick={() => {
+            const topRated = restaurentlist.filter((restaurentList) => restaurentList.info.avgRatingString > 4)
+            console.log(topRated);
+            setRestaurentList(topRated);
+          }
+          }>
           Top Rated
         </a>
       </div>
