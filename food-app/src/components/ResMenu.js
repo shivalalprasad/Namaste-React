c361fba0ea1deb52c611e6a0d2174270c4fea34c
Your Name <you@example.com>
2024-09-25 18:57:15 +0530

    added shimmer UI

--- food-app/src/components/ResMenu.js
+++ food-app/src/components/ResMenu.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { ITEM_IMG, RES_IMG } from "../util/constants"
 import { useParams } from "react-router-dom"
+import SuiResMenu from "./shimmerui/SuiResMenu"
 
 
 const ResMenu = () => {
@@ -71,7 +72,7 @@ const ResMenu = () => {
     // console.log(resItemsData1);
   }
 
-  return (
+  return resData.length == 0 || resData == undefined ? <SuiResMenu /> : (
     <>
       <h1 className="text-3xl text-center text-bolder my-6">{resData?.data?.cards[2]?.card?.card?.info.name}</h1>
       <section className="text-gray-600 body-font">
@@ -81,7 +82,7 @@ const ResMenu = () => {
               // <h1 key={item.id} className="text-center w-full text-3xl font-bolder"> {"hello"} </h1>,
               item.card.card.itemCards == undefined ? <h1 className="text-center w-full text-3xl font-bolder"></h1> : item.card.card.itemCards.map((item) => (
                 <div key={item.card?.info?.id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                  <a className="block relative h-60 rounded overflow-hidden">
+                  <a className="block bg-gray-300 relative h-60 rounded overflow-hidden">
                     <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={item.card?.info?.imageId ? ITEM_IMG + item.card?.info?.imageId : RES_IMG + resData?.data?.cards[2]?.card?.card?.info.cloudinaryImageId} />
                     {console.log(item)}
                   </a>
---
+++ food-app/src/components/shimmerui/SuiResMenu.js
@@ -0,0 +1,133 @@
+
+const SuiResMenu = () => {
+  return (
+    <section className="text-gray-600  body-font">
+      <div className="container px-5 py-24 bg-gray-100 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-gray-200">
+            <a className="block relative h-60 rounded overflow-hidden bg-gray-300 rounded">
+              <div alt="ecommerce" className=" bg-gray-300 object-cover object-center w-full h-60 block" src="https://dummyimage.com/420x260"></div>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 h-4 w-32 text-xs tracking-widest title-font mb-1 bg-gray-500 rounded-lg"></h3>
+              <h2 className="text-gray-900 h-4 w-24 title-font text-lg font-medium bg-gray-400 rounded-lg"> </h2>
+              <p className="h-4 w-12 bg-gray-300 mt-1 rounded-lg"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SuiResMenu;
